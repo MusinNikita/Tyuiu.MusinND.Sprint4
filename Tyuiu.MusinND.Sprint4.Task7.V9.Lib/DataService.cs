b79f96244aadf6ff7ede2581fec1d8986ea6d0a8
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:22:04 +0500

    1212

---
+++ Tyuiu.MusinND.Sprint4.Task7.V9.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MusinND.Sprint4.Task7.V9.Lib
+{
+    public class DataService : ISprint4Task7V9
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            // Проверка, достаточно ли символов в строке для создания матрицы
+            if (value.Length < n * m)
+            {
+                throw new ArgumentException("Строка не содержит достаточного количества цифр для формирования матрицы.");
+            }
+
+            // Создаем матрицу с помощью Array
+            var matrix = Array.CreateInstance(typeof(int), n, m);
+            int evenCount = 0; // Счетчик четных чисел
+
+            // Заполняем матрицу
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    // Преобразуем символ в целое число и заполняем матрицу
+                    int valueToInsert = value[i * m + j] - '0'; // Конвертируем символ в целое число
+                    matrix.SetValue(valueToInsert, i, j); // Устанавливаем значение в матрицу
+
+                    // Проверяем, является ли число четным
+                    if (valueToInsert % 2 == 0)
+                    {
+                        evenCount++; // Увеличиваем счетчик четных чисел
+                    }
+                }
+            }
+
+            return evenCount; // Возвращаем количество четных чисел
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint4.Task7.V9/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MusinND.Sprint4.Task7.V9.Lib;
+namespace Tyuiu.MusinND.Sprint4.Task7.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Мусин Никита Денисович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #9                                                            *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дана строка из одноразрядных цифр "864299753". Преобразуйте ее в матрицу *");
+            Console.WriteLine("* 3 на 3 и подсчитайте количество четных чисел.                           *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

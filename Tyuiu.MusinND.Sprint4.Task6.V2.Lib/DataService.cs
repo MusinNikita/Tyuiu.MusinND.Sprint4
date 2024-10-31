f42a5fc5dd67b46b0bb5d7581a3a0e7667b258f9
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:19:38 +0500

    12121

--- Tyuiu.MusinND.Sprint4.Task6.V2.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint4.Task6.V2.Lib/DataService.cs
@@ -6,18 +6,9 @@ namespace Tyuiu.MusinND.Sprint4.Task6.V2.Lib
     {
         public string[] Calculate(string[] array)
         {
-            List<string> resultList = new List<string>(); // Список для хранения элементов длиной более 5 символов
-
-            // Проходим по всем элементам массива
-            foreach (string item in array)
-            {
-                if (item.Length > 5) // Проверяем, если длина элемента больше 5
-                {
-                    resultList.Add(item); // Добавляем элемент в список
-                }
-            }
-
-            return resultList.ToArray(); // Преобразуем список в массив и возвращаем его
+            // Используем метод Array.FindAll для фильтрации элементов
+            string[] result = Array.FindAll(array, item => item.Length > 5);
+            return result; // Возвращаем отфильтрованный массив
         }
     }
 }

568b27997044f7c34bc4899a4cc952366ab65178
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:18:46 +0500

    1212

---
+++ Tyuiu.MusinND.Sprint4.Task6.V2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MusinND.Sprint4.Task6.V2.Lib;
+namespace Tyuiu.MusinND.Sprint4.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Мусин Никита Денисович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Класс Array                                                             *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #2                                                            *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан строковый массив данных ["Белый", "Черный", "Зеленый", "Синий", "Крас*");
+            Console.WriteLine("*сный", "Желтый", "Фиолетовый"], используя класс Array, выведите элементы *");
+            Console.WriteLine("* массива, длина которых больше 5 символов.                               *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

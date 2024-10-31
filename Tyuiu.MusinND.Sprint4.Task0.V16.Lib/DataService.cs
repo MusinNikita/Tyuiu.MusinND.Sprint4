c3fedcc4d05f53f8b90bef02bcef30bfc5502a14
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:00:00 +0500

    1121212

---
+++ Tyuiu.MusinND.Sprint4.Task0.V16.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MusinND.Sprint4.Task0.V16.Lib
+{
+    public class DataService : ISprint4Task0V16
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int product = 1;
+            bool hasEven = false; // Флаг для проверки, есть ли четные элементы
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0) // Проверяем, является ли число четным
+                {
+                    product *= num; // Умножаем четное число на произведение
+                    hasEven = true; // Устанавливаем флаг, что четные элементы найдены
+                }
+            }
+
+            return hasEven ? product : 0; // Если четных элементов не было, возвращаем 0
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint4.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint4.Task0.V16.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

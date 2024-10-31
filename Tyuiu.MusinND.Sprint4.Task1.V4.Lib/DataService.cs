8434d978adbd17f818bea3cff728c53c7784c5e0
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:06:17 +0500

    1212

---
+++ Tyuiu.MusinND.Sprint4.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MusinND.Sprint4.Task1.V4.Lib
+{
+    public class DataService : ISprint4Task1V4
+    {
+        public int Calculate(int[] array)
+        {
+            int product = 1; // Инициализация произведения
+            bool hasOdd = false; // Флаг для проверки наличия нечетных элементов
+
+            foreach (int num in array)
+            {
+                if (num % 2 != 0) // Проверяем, является ли число нечетным
+                {
+                    product *= num; // Умножаем нечетное число на произведение
+                    hasOdd = true; // Устанавливаем флаг, что нечетные элементы найдены
+                }
+            }
+
+            return hasOdd ? product : 0; // Если нечетных элементов не было, возвращаем 0
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint4.Task1.V4/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MusinND.Sprint4.Task1.V4.Lib;
+namespace Tyuiu.MusinND.Sprint4.Task1.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Мусин Никита Денисович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Одномерные массивы. (ввод с клавиатуры)                                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПБ-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан одномерный целочисленный массив на 10 элементов заполненный значениям*");
+            Console.WriteLine("*ми с клавиатуры в диапазоне от 1 до 9 подсчитать произведение нечетных эл*");
+            Console.WriteLine("*лементов массива.  С клавиатуры: 1, 2, 5, 3, 4, 7, 8, 8, 3, 9            *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

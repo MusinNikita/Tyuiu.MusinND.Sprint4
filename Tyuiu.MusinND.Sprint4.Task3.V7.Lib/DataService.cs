a7f96a9451f7475a3c3a089cbb22addc634ac03f
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-31 21:12:28 +0500

    12121212

---
+++ Tyuiu.MusinND.Sprint4.Task3.V7.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MusinND.Sprint4.Task3.V7.Lib
+{
+    public class DataService : ISprint4Task3V7
+    {
+        public int Calculate(int[,] array)
+        {
+            int count = 0; // Переменная для подсчета нечетных элементов
+
+            // Проходим по всем элементам двумерного массива
+            for (int i = 0; i < array.GetLength(0); i++) // Проходим по строкам
+            {
+                for (int j = 0; j < array.GetLength(1); j++) // Проходим по столбцам
+                {
+                    if (array[i, j] % 2 != 0) // Проверяем, является ли элемент нечетным
+                    {
+                        count++; // Увеличиваем счетчик
+                    }
+                }
+            }
+
+            return count; // Возвращаем количество нечетных элементов
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint4.Task3.V7/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.MusinND.Sprint4.Task3.V7.Lib;
+namespace Tyuiu.MusinND.Sprint4.Task3.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Мусин Никита Денисович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Двумерные массивы. (статический ввод)                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #7                                                            *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статичес*");
+            Console.WriteLine("*скими значениями в диапазоне от 1 до 9. Подсчитайте количество нечетных э*");
+            Console.WriteLine("*элементов во всем массиве. 9, 6, 9, 3, 7, 3, 3, 3, 8, 2, 2, 1, 3, 5, 2, 6*");
+            Console.WriteLine("*6, 2, 3, 2, 5, 4, 5, 6, 9, 5                                             *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
